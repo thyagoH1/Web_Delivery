de0e45c8e80219513e82a4c7bc8929b3802181e5
thyagoH1 <thyago_tk2010@hotmail.com>
2019-05-17 14:10:26 -0300

    primeiro commit repositorio

---
+++ Git_Repositorio/DeliveryPizzaria/src/main/java/br/com/DeliveryPizzaria/DeliveryPizzariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.DeliveryPizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPizzariaApplication.class, args);
+	}
+
+}
